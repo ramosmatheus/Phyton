56c400a97b3bb5e846e90556fac1bb6180ee4645
Aluno <ramosmatheusc@gmail.com>
2019-03-25 21:04:21 -0300

    Configurações

---
+++ Adotar/animais/urls.py
@@ -0,0 +1,10 @@
+# módulo do Django para criar urls
+from django.urls import path
+
+# Importe todas suas classes do view
+from .views import *
+
+urlpatterns = [
+    #path('caminho/da/url', ClasseLáDoView.as_view(), name="nomeDessaURL")
+    path('', PaginaInicialView.as_view(), name="sobre"),
+]
--- Adotar/animais/views.py
+++ Adotar/animais/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from django.views.generic import TemplateView
+
 # Create your views here.
+
+class PaginaInicialView(TemplateView):
+    #Nome do arquivo que vai ser utilizado para renderisar esta página/método/classe
+    template_name = "inicial.html"
+
+class SobreInicialView(TemplateView):
+    #Nome do arquivo que vai ser utilizado para renderisar esta página/método/classe
+    template_name = "sobre.html"
